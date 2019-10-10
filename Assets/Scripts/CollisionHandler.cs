b8fa6d496d0944d87d6fc9591d5352ecfbffc9bf
ldev <luisdegan10@hotmail.com>
2019-10-10 11:46:04 -0300

    initial

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] private float levelLoadDelay = 1f;
+    [SerializeField] private GameObject deatFX;
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        DeathSequence();
+        deatFX.SetActive(true);
+        Invoke(nameof(ReloadScene), levelLoadDelay);
+    }
+
+    private void DeathSequence()
+    {
+        SendMessage("OnPlayerDeath");
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(LoadGame), 3);
+    }
+
+    private void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
